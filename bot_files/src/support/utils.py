06551e487936a119382be6af0dcfaf02728ae699
Teodoro Bertolozzi Mendes <teobmendes@gmail.com>
2021-04-05 10:43:25 -0300

    showing hours instead of days report compiler

--- bot_files/src/support/utils.py
+++ bot_files/src/support/utils.py
@@ -1,9 +1,7 @@
-from datetime import timedelta
 from typing import Union
 
 
 def seconds_to_str(seconds: Union[int, float]):
     seconds = int(seconds)
-    d = str(timedelta(seconds=seconds))
-    d = d.split(':')
+    d = seconds//3600, (seconds//60) % 60
     return f"{d[0]}h{d[1]}m"
